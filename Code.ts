cf4fb11d5611f1b261f95ecd4581aac64a193727
jacobvdb <jacob@bkper.com>
2020-09-30 08:54:13 -0300

    several changes for testing

--- Code.ts
+++ Code.ts
@@ -1,5 +1,14 @@
 
 function doGet(e) {
   var params = JSON.stringify(e);
-  return HtmlService.createHtmlOutput(params);
+
+  var bookId = e.parameter.bookId;
+  var query = e.parameter.query
+
+  var book = BkperApp.getBook(bookId)
+  var bookName = book.getName()
+
+  Logger.log(bookName)
+
+  //return HtmlService.createHtmlOutput("Book: " + bookId +" query: " +query);
 }
